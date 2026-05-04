fd5d69bd6a4a8f06ac8dc3fac39e1ed3e64b2f27
Samir Cifuentes <scifuentesp2@est.ups.edu.ec>
2026-05-04 15:13:52 -0500

    Commit Inicial

---
+++ script.py
@@ -0,0 +1,11 @@
+#---------------------------------------------------------------
+# Script de Python para probar GIT
+# Autor: Samir Cifuentes <scifuentesp2@est.ups.edu.ec>
+# Fecha: 04/05/2026
+#---------------------------------------------------------------
+def main():
+    nombre=input("Cómo te llamas: ")
+    print("Hola",nombre,"bienvenido a mi mundo")
+
+if __name__ == "__main__":
+        main()
