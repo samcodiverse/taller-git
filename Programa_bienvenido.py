02933feae45c9ad0a692513480edc95332fde042
Samir Cifuentes <scifuentesp2@est.ups.edu.ec>
2026-05-04 16:03:32 -0500

    Programa ASCII

---
+++ Programa_bienvenido.py
@@ -0,0 +1,26 @@
+#---------------------------------------------------------------
+# Script de Python como primer programa
+# Autor: Samir Cifuentes <scifuentesp2@est.ups.edu.ec>
+# Fecha: 04/05/2026
+#---------------------------------------------------------------
+def main():
+    print(r""" ALTO IDENTIFIQUESE                              /         \
+      (_V__V_)                (_\_(___)_/_)
+        (oo)                      (o o)
+ /-------\/                        \ /----------\\
+"| _____||                          O||        ||~
+ ||     ||                           ||------/@||
+  ^^     ^^                          ||        ||     
+                         ^^        ^^""")
+    
+    nombre = input("Ingresa tu nombre : ")
+    edad = input("Ingresa tu edad: ")
+    
+    print("Hola un placer en conocerte", nombre, "de edad terricola:", edad)
+    
+    print(r"""  o  \ o / _ o       __|   \ /    |__      o _ \ o /  o
+ /|\   |    /\  ___\o  \o   |   o/   o/__  /\    |   /|\
+ / \  / \  | \ /)  |   ( \ /o\ / )   |  (\ / |  / \  / \\""")  # ← aquí está el cambio
+
+if __name__ == "__main__":
+    main()
